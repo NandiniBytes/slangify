fff98fef14eb1f63d6b8846cc08fce1ae4da92fa
NandiniBytes <22bcm042@nirmauni.ac.in>
2025-06-02 18:40:33 +0530

    Update manage.py

--- slangify_project/manage.py
+++ slangify_project/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'slangify_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'slangify_production.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
