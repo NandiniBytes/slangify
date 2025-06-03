a4797bd05b99b5e8b38562402a160b230997c5ab
NandiniBytes <22bcm042@nirmauni.ac.in>
2025-06-03 10:59:45 +0530

    Update urls.py

--- slangify_project/slangify_production/urls.py
+++ slangify_project/slangify_production/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("OK", status=200)
 
 urlpatterns = [
+    path('', health_check, name='health-check'),  # Root URL
+    path('admin/', admin.site.urls),
     path('api/', include('slangify.urls')),
 ]
